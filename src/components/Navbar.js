804c23c2c6e19c4e42ca27d632171a7b6c2d3412
zckoh <zckoh96@gmail.com>
2020-02-11 18:38:17 +0000

    change navbar to fuctional component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import {
     MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler,
     MDBCollapse, MDBNavItem, MDBNavLink,
@@ -7,54 +7,72 @@ import {
 import Logo from '../assets/img/logo_transparent.png';
 import '../assets/css/navbar.css';
 
-class MyNavbar extends React.Component {
-    state = {
-        collapseID: ""
+const MyNavbar = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    const toggleCollapse = () => { setCollapsed(!collapsed); }   
+    
+    const node = useRef();
+
+    const handleClickOutside = e => {
+        if (node.current.contains(e.target)) {
+          // inside click
+          return;
+        }
+        // outside click
+        setCollapsed(false);
     };
 
-    toggleCollapse = collapseID => () =>
-        this.setState(prevState => ({
-            collapseID: prevState.collapseID !== collapseID ? collapseID : ""
-        }));
+    useEffect(() => {
+        if (collapsed) {
+          document.addEventListener("mousedown", handleClickOutside);
+        } else {
+          document.removeEventListener("mousedown", handleClickOutside);
+        }
+    
+        return () => {
+          document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [collapsed]);
 
-    render() {
-        return (
-            <MDBNavbar color="stylish-color-dark" fixed="top" dark expand="md" scrolling>
-                <MDBNavbarBrand href="home">
-                    <img  src={Logo} style={{ 
-                        height: '1.5rem',
-                        width: '1.5rem',
-                        "marginBottom": '0.5rem',
-                        "marginRight": '0.5rem', }} alt="fireSpot"/>
-                    <strong className="white-text">ZCK Website</strong>
-                </MDBNavbarBrand>
-                <MDBNavbarToggler onClick={this.toggleCollapse("navbarCollapse")} />
-                <MDBCollapse id="navbarCollapse" isOpen={this.state.collapseID} navbar>
-                    <MDBNavbarNav left>
-                        <MDBNavItem>
-                            <MDBNavLink to={"home"}><strong>Home</strong></MDBNavLink>
-                        </MDBNavItem>
-                        <MDBNavItem>
-                            <MDBNavLink to={"projects"}><strong>Projects</strong></MDBNavLink>
-                        </MDBNavItem>
-                        <MDBNavItem>
-                            <MDBNavLink to={"what-to-eat"}><strong>What To Eat!</strong></MDBNavLink>
-                        </MDBNavItem>
-                        <MDBNavItem>
-                            <MDBDropdown className="mydropdown">
-                                <MDBDropdownToggle nav caret>
-                                    <span className="mr-2"><strong>Apps</strong></span>
-                                </MDBDropdownToggle>
-                                <MDBDropdownMenu className="mydropdown">
-                                    <MDBDropdownItem className="mydropdownitems" href="#!">More coming soon!</MDBDropdownItem>
-                                </MDBDropdownMenu>
-                            </MDBDropdown>
-                        </MDBNavItem>
-                    </MDBNavbarNav>
-                </MDBCollapse>
-            </MDBNavbar>
-        );
-    }
+    return (
+        <div ref={node} >
+        <MDBNavbar color="stylish-color-dark" fixed="top" dark expand="md" scrolling>
+        <MDBNavbarBrand href="home">
+            <img  src={Logo} style={{ 
+                height: '1.5rem',
+                width: '1.5rem',
+                "marginBottom": '0.5rem',
+                "marginRight": '0.5rem', }} alt="fireSpot"/>
+            <strong className="white-text">ZCK Website</strong>
+        </MDBNavbarBrand>
+        <MDBNavbarToggler onClick={() => {toggleCollapse()} } />
+        <MDBCollapse id="navbarCollapse" isOpen={collapsed} navbar>
+            <MDBNavbarNav left>
+                <MDBNavItem>
+                    <MDBNavLink to={"home"}><strong>Home</strong></MDBNavLink>
+                </MDBNavItem>
+                <MDBNavItem>
+                    <MDBNavLink to={"projects"}><strong>Projects</strong></MDBNavLink>
+                </MDBNavItem>
+                <MDBNavItem>
+                    <MDBNavLink to={"what-to-eat"}><strong>What To Eat!</strong></MDBNavLink>
+                </MDBNavItem>
+                <MDBNavItem>
+                    <MDBDropdown className="mydropdown">
+                        <MDBDropdownToggle nav caret>
+                            <span className="mr-2"><strong>Apps</strong></span>
+                        </MDBDropdownToggle>
+                        <MDBDropdownMenu className="mydropdown">
+                            <MDBDropdownItem className="mydropdownitems" href="#!">More coming soon!</MDBDropdownItem>
+                        </MDBDropdownMenu>
+                    </MDBDropdown>
+                </MDBNavItem>
+            </MDBNavbarNav>
+        </MDBCollapse>
+    </MDBNavbar>
+    </div>
+
+    );
 }
 
 export default MyNavbar;
